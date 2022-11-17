e581aad49d5c6e2c900e05b10feafda4009246da
IMFORM-MM-2004 <imform-mm-2004@IMFORM-MM-2004.local>
2022-11-17 09:40:59 +0900

    React library

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,8 +1,45 @@
+import { useState } from "react"
+
+  const Container = ()=>{
+    console.log("컨테이너 업로드")
+    return (
+      <div>컨테이너</div>
+    )
+  }
+  const Presenter1 = ()=>{
+    console.log("프리젠터1 업로드")
+    const [state,setState] = useState(true)
+    const onClick = ()=>{
+      setState(false)
+    }
+    return (
+      <div>프리젠터1
+        {state}
+        <button onClick={onClick}>stateChange!@</button>
+      </div>
+
+    )
+  }
+
+  const Presenter2 = ()=>{
+    console.log("프리젠터2 업로드")
+    return (
+      <div>프리젠터2
+        <Presenter1/>
+      </div>
+    )
+  }
 
 
 function App() {
+
+
   return (
-<div>클릭</div>
+    <>
+<Container/>
+<Presenter2/>
+
+    </>
   )
 }
 
